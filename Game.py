87253dafee1097d28c572a2d754d484bcd58e6ad
Whatruska <vitdub2@gmail.com>
2020-10-10 23:13:42 +0300

    OOP git add .

---
+++ Game.py
@@ -0,0 +1,195 @@
+import os
+import sys
+
+import pygame
+import pygame_menu
+
+import Settings
+from Player import Player
+from pygame.locals import *
+
+class Game:
+
+    def __init__(self, map_path="map", music_path="music.wav"):
+        pygame.mixer.pre_init(44100, -16, 2, 512)
+        pygame.mixer.init()
+
+        self.__display = pygame.Surface(Settings.DISPLAY_SIZE)
+
+        self.__game_map = self.__load_map(map_path)
+
+        self.__tile_rects = []
+        self.__tiles_imgs = self.__load_tiles_imgs()
+        self.__spikes = []
+
+        self.__music_path = music_path
+
+        self.__grass_sound_timer = 0
+
+        self.__true_scroll = [0, 0]
+        self.__scroll = []
+
+        self.__clock = pygame.time.Clock()
+
+        self.__screen = pygame.display.set_mode(Settings.WINDOW_SIZE, 0, 32)
+
+        self.__player = Player(self)
+
+        flag_x, flag_y = Settings.END_FLAG_POINT
+        self.__end_flag_zone = [
+            pygame.Rect(flag_x * Settings.TILE_SIZE, (flag_y + 1) * Settings.TILE_SIZE, Settings.TILE_SIZE, Settings.TILE_SIZE),
+            pygame.Rect(flag_x * Settings.TILE_SIZE, (flag_y - 1) * Settings.TILE_SIZE, Settings.TILE_SIZE, Settings.TILE_SIZE),
+            pygame.Rect((flag_x - 1) * Settings.TILE_SIZE, flag_y * Settings.TILE_SIZE, Settings.TILE_SIZE, Settings.TILE_SIZE),
+            pygame.Rect((flag_x + 1) * Settings.TILE_SIZE, flag_y * Settings.TILE_SIZE, Settings.TILE_SIZE, Settings.TILE_SIZE),
+        ]
+
+    def get_tile_rects(self):
+        return self.__tile_rects
+
+    def get_spikes(self):
+        return self.__spikes
+
+    def get_end_flag_zone(self):
+        return self.__end_flag_zone
+
+    def get_display(self):
+        return self.__display
+
+    def get_scroll(self):
+        return self.__scroll
+
+    def create_win_menu(self):
+        menu = pygame_menu.Menu(Settings.WINDOW_SIZE[1], Settings.WINDOW_SIZE[0], 'You won', theme=pygame_menu.themes.THEME_DARK)
+
+        menu.add_button('Play again', self.__start_game)
+        menu.add_button('Quit', pygame_menu.events.EXIT)
+        menu.mainloop(self.__screen)
+
+    def __create_start_menu(self):
+        menu = pygame_menu.Menu(Settings.WINDOW_SIZE[1], Settings.WINDOW_SIZE[0], 'Welcome',theme=pygame_menu.themes.THEME_DARK)
+
+        menu.add_button('Play', self.__start_game)
+        menu.add_button('Quit', pygame_menu.events.EXIT)
+        menu.mainloop(self.__screen)
+
+    def __load_map(self, path):
+        with open(os.path.join(Settings.CURR_PATH, path + ".txt"), 'r') as f:
+            data = f.read()
+        data = data.split('\n')
+        game_map = []
+        for row in data:
+            tiles = row.split(' ')
+            game_map.append(list(tiles))
+
+        return game_map
+
+    def __load_tiles_imgs(self):
+        tiles = dict()
+        for i in range(0, 29):
+            formatted = '%02d' % i
+            tiles[formatted] = pygame.image.load(os.path.join(Settings.CURR_PATH, 'tiles/%s.png' % formatted))
+
+        return tiles
+
+    def start(self):
+        pygame.init()
+        pygame.display.set_caption(Settings.GAME_NAME_LABEL)
+
+        #Setting up the music
+        pygame.mixer.music.load(os.path.join(Settings.CURR_PATH, self.__music_path))
+        pygame.mixer.music.play(-1)
+
+        self.__init_map()
+
+        self.__create_start_menu()
+
+    def __draw_background(self):
+        pygame.draw.rect(self.__display, Settings.BACKGROUND_COLOR, pygame.Rect(0, 120, 300, 80))
+
+        for background_object in Settings.BACKGROUND_OBJECTS_DATA:  # some background object
+            obj_rect = pygame.Rect(background_object[1][0] - self.__scroll[0] * background_object[0],
+                                   background_object[1][1] - self.__scroll[1] * background_object[0], background_object[1][2],
+                                   background_object[1][3])
+            if background_object[0] == 0.5:
+                pygame.draw.rect(self.__display, (14, 222, 150), obj_rect)
+            else:
+                pygame.draw.rect(self.__display, (9, 91, 85), obj_rect)
+
+    def __count_scroll(self):
+        self.__true_scroll[0] += (self.__player.get_player_rect().x - self.__true_scroll[
+            0] - Settings.WIDTH_FOR_PERSON_CENTER) / 20  # / 20 this effect of camera move
+        self.__true_scroll[1] += (self.__player.get_player_rect().y - self.__true_scroll[
+            1] - Settings.HEIGHT_FOR_PERSON_CENTER) / 20
+
+        self.__scroll = [int(x) for x in self.__true_scroll]
+
+    def __init_map(self):
+        y = 0
+        for layer in self.__game_map:
+            x = 0
+            for tile in layer:
+                if tile in Settings.SPIKE_NUMBERS:
+                    self.__spikes.append(pygame.Rect(x * Settings.TILE_SIZE, y * Settings.TILE_SIZE, Settings.TILE_SIZE,
+                                              Settings.TILE_SIZE))
+                elif tile != '**':
+                    self.__tile_rects.append(pygame.Rect(x * Settings.TILE_SIZE, y * Settings.TILE_SIZE, Settings.TILE_SIZE,
+                                                  Settings.TILE_SIZE))
+                x += 1
+            y += 1
+
+    def __redraw_map(self):
+        y = 0
+        for layer in self.__game_map:
+            x = 0
+            for tile in layer:
+                if tile != '**':
+                    self.__display.blit(self.__tiles_imgs[tile],(x * Settings.TILE_SIZE - self.__scroll[0], y * Settings.TILE_SIZE - self.__scroll[1]))  # 16 this width and height our png
+                x += 1
+            y += 1
+
+    def __start_game(self):
+        while True: # game loop
+            self.__display.fill(Settings.SKY_COLOR)  # clear screen by filling it with blue all environment will be here
+
+            if self.__grass_sound_timer > 0:
+                self.__grass_sound_timer -= 10
+
+            self.__count_scroll()
+
+            self.__draw_background()
+
+            self.__redraw_map()
+
+            self.__player.redraw()
+
+            for event in pygame.event.get():  # event loop
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        menu = pygame_menu.Menu(Settings.WINDOW_SIZE[0], Settings.WINDOW_SIZE[1], 'Pause', theme=pygame_menu.themes.THEME_DARK)
+
+                        menu.add_button('Continue', self.__start_game)
+                        menu.add_button('Quit', pygame_menu.events.EXIT)
+                        menu.mainloop(self.__screen)
+
+                    if event.key == K_1:  # off music
+                        pygame.mixer.music.fadeout(1000)
+                    if event.key == K_2:  # on music
+                        pygame.mixer.music.play(-1)
+                    if event.key == K_RIGHT:
+                        self.__player.move_right()
+                    if event.key == K_LEFT:
+                        self.__player.move_left()
+                    if event.key == K_UP:
+                        self.__player.jump()
+                if event.type == KEYUP:
+                    if event.key == K_RIGHT:
+                        self.__player.stop_right()
+                    if event.key == K_LEFT:
+                        self.__player.stop_left()
+
+            self.__screen.blit(pygame.transform.scale(self.__display, Settings.WINDOW_SIZE), (0, 0))  # rendering game session
+            pygame.display.update()  # update display
+            self.__clock.tick(60)  # maintain 60fps
---
+++ Player.py
@@ -0,0 +1,197 @@
+import os
+import random
+
+import pygame
+
+import Settings
+
+PLAYER_RECT_WIDTH = 6
+PLAYER_RECT_HEIGHT = 13
+
+
+class Player:
+    def __init__(self, game, x=Settings.SPAWN_X, y=Settings.SPAWN_Y, jump_sound_path="jump.wav", grass_sound_pathes=["grass_0.wav", "grass_1.wav"]):
+
+        self.__rect = pygame.Rect(x, y, PLAYER_RECT_WIDTH, PLAYER_RECT_HEIGHT)
+
+        loaded_sounds = self.__load_sounds(jump_sound_path, grass_sound_pathes)
+
+        self.__jump_sound = loaded_sounds[0]
+        self.__grass_sounds = loaded_sounds[1]
+
+        self.__moving_right = False
+        self.__moving_left = False
+
+        self.__vertical_momentum = 0
+
+        self.__air_timer = 0
+
+        self.__grass_sound_timer = 0
+
+        self.__animation_frames = {}
+
+        self.__animation_database = {}
+
+        self.__animation_database['run'] = self.__load_animation('player_animations/run', [4, 4, 4, 4])
+        self.__animation_database['idle'] = self.__load_animation('player_animations/idle', [7, 7, 40])
+
+        self.__player_action = 'idle'
+        self.__player_frame = 0
+        self.__player_flip = False
+
+        self.__game = game
+
+    def get_player_rect(self):
+        return self.__rect
+
+    def __change_action(self, action_var, frame, new_value):
+        if action_var != new_value:
+            action_var = new_value
+            frame = 0
+        return action_var, frame
+
+    def __move(self, rect, movement, tiles, spikes):
+        collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+
+        self.__collide_spikes(rect, spikes)
+
+        self.__collide_flag(rect, end_flag_zone=self.__game.get_end_flag_zone())
+
+        rect.x += movement[0]
+        hit_list = self.__collision_test(rect, tiles)
+
+        for tile in hit_list:
+            # restriction on movement in right and take value from mirror object edge
+            if movement[0] > 0:
+                rect.right = tile.left
+                collision_types['right'] = True
+            elif movement[0] < 0:
+                rect.left = tile.right
+                collision_types['left'] = True
+
+        rect.y += movement[1]
+        hit_list = self.__collision_test(rect, tiles)
+
+        for tile in hit_list:
+            if movement[1] > 0:
+                rect.bottom = tile.top
+                collision_types['bottom'] = True
+            elif movement[1] < 0:
+                rect.top = tile.bottom
+                collision_types['top'] = True
+        return rect, collision_types
+
+    def __move_to_spawn(self, rect):
+        rect.x = Settings.SPAWN_X
+        rect.y = Settings.SPAWN_Y
+
+    def __collision_test(self, rect, tiles):  # use for collect object which colliderect by our person
+        hit_list = []
+        for tile in tiles:
+            if rect.colliderect(tile):  # touch person with object by area 3-4
+                hit_list.append(tile)
+        return hit_list
+
+    def __collide_flag(self, rect, end_flag_zone):
+        for flag_zone_item in end_flag_zone:
+            if rect.colliderect(flag_zone_item):
+                self.__move_to_spawn(rect)
+                self.__game.create_win_menu()
+
+    def __collide_spikes(self, rect, spikes):
+        for spike in spikes:
+            if rect.colliderect(spike):
+                self.__move_to_spawn(rect)
+
+    def __load_animation(self, path, frame_durations):
+        animation_name = path.split('/')[-1]
+        animation_frame_data = []
+        n = 0
+        for frame in frame_durations:
+            animation_frame_id = animation_name + '_' + str(n)
+            img_loc = path + '/' + animation_frame_id + '.png'
+            # player_animations/idle/idle_0.png
+            animation_image = pygame.image.load(os.path.join(Settings.CURR_PATH, img_loc)).convert()
+            animation_image.set_colorkey((255, 255, 255))
+            self.__animation_frames[animation_frame_id] = animation_image.copy()
+            for i in range(frame):
+                animation_frame_data.append(animation_frame_id)
+            n += 1
+        return animation_frame_data
+
+    def __load_sounds(self, jump_sound_path, grass_sound_pathes):
+        jump_sound = pygame.mixer.Sound(os.path.join(Settings.CURR_PATH, jump_sound_path))
+        grass_sounds = [
+            pygame.mixer.Sound(os.path.join(Settings.CURR_PATH, grass_sound_pathes[0])),
+            pygame.mixer.Sound(os.path.join(Settings.CURR_PATH, grass_sound_pathes[1]))
+        ]
+        grass_sounds[0].set_volume(0.1)
+        grass_sounds[1].set_volume(0.1)
+
+        return (jump_sound, grass_sounds)
+
+    def move_right(self):
+        self.__moving_right = True
+
+    def move_left(self):
+        self.__moving_left = True
+
+    def stop_right(self):
+        self.__moving_right = False
+
+    def stop_left(self):
+        self.__moving_left = False
+
+    def jump(self):
+        if self.__air_timer < 6:
+            self.__jump_sound.play()
+            self.__vertical_momentum = -5
+
+    def redraw(self):
+        #  place with our person
+        player_movement = [0, 0]
+
+        player_flip = False
+
+        #  move x
+        if self.__moving_right:
+            player_movement[0] += 2
+        if self.__moving_left:
+            player_movement[0] -= 2
+
+        #  move y
+        player_movement[1] += self.__vertical_momentum
+        self.__vertical_momentum += 0.25
+        if self.__vertical_momentum > 3:
+            self.__vertical_momentum = 3
+
+        # This need to change animation when person run
+        if player_movement[0] == 0:
+            self.__player_action, self.__player_frame = self.__change_action(self.__player_action, self.__player_frame, 'idle')
+        if player_movement[0] > 0:
+            player_flip = False
+            self.__player_action, self.__player_frame = self.__change_action(self.__player_action, self.__player_frame, 'run')
+        if player_movement[0] < 0:
+            player_flip = True
+            self.__player_action, self.__player_frame = self.__change_action(self.__player_action, self.__player_frame, 'run')
+
+        self.__rect, collisions = self.__move(self.__rect, player_movement, self.__game.get_tile_rects(), self.__game.get_spikes())
+
+        if collisions['bottom']:
+            self.__air_timer = 0
+            self.__vertical_momentum = 0
+            if player_movement[0] != 0:  # play_grass_sound when you moved
+                if self.__grass_sound_timer == 0:
+                    self.__grass_sound_timer = 30
+                    random.choice(self.__grass_sounds).play()
+        else:
+            self.__air_timer += 1  # jump timer
+
+        self.__player_frame += 1
+        if self.__player_frame >= len(self.__animation_database[self.__player_action]):  # this need to restart animation of our person
+            self.__player_frame = 0
+
+        player_img_id = self.__animation_database[self.__player_action][self.__player_frame]
+        player_img = self.__animation_frames[player_img_id]  # animation when person stand on
+        scroll = self.__game.get_scroll()
+        self.__game.get_display().blit(pygame.transform.flip(player_img, player_flip, False), (self.__rect.x - scroll[0], self.__rect.y - scroll[1]))  # object rendering .flip need for change side of run when render
